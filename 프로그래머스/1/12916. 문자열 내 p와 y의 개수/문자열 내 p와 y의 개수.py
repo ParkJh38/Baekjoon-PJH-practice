b1300ad80df6f8461d432c7ef0411f1d6ac97bef
JH Park <74255823+ParkJh38@users.noreply.github.com>
2024-10-27 23:43:54 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,34 +1,15 @@
-# def solution(s):
-#     alph_count = {'p':0, 'y':0}
-
-#     s = ''
-#     s = s.lower()
-#     for i in range(len(s)):
-#         if s[i] == p:
-#             alph_count['p'] += 1
-#         elif s[i] == y:
-#             alph_count['y'] += 1
-#     if alph_count['p'] == alph_count['y']:
-#         print(alph_count['p'])
-#         return True
-#     elif alph_count['p'] != alph_count['y']:
-#         return False
-#     elif alph_count['p'] == 0 and alph_count['y'] == 0:
-#         return True
-
 def solution(s):
-    alph_count = {'p': 0, 'y': 0}
-    
-    s = s.lower()
-    for char in s:
-        if char == 'p':
-            alph_count['p'] += 1
-        elif char == 'y':
-            alph_count['y'] += 1
-
-    if alph_count['p'] == alph_count['y']:
+    count_char = {'p': 0, 'y': 0}
+    s= s.lower()
+    for i in range(len(s)):
+        if s[i] == 'p':
+            count_char['p'] += 1
+        elif s[i] == 'y':
+            count_char['y'] += 1         
+    if count_char['p'] == count_char['y']:
         return True
-    elif alph_count['p'] != alph_count['y']:
+    elif count_char['p'] == 0 and count_char['y'] == 0:
+        return True
+    else:
         return False
-    elif alph_count['p'] == 0 & alph_count['y'] == 0:
-        return True
+    
