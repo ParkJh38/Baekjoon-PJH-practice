eb8871f1666d6277d7a0ae737d8806b1ececc390
JH Park <74255823+ParkJh38@users.noreply.github.com>
2024-10-27 20:59:52 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,9 +1,10 @@
 def solution(s):
     if len(s) % 2 == 0: #길이 짝수
         k = len(s)//2 - 1
-        answer1 = s[k]
-        answer2 = s[k+1]
-        answer = answer1 + answer2
+        #answer1 = s[k]
+        #answer2 = s[k+1]
+        #answer = answer1 + answer2
+        answer = s[k : k+2]
     else: #길이 홀수
         answer = s[len(s)//2]
         
