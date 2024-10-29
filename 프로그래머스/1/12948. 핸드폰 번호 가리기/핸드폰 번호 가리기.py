7677291fae941d051d4d93df930af64d54ea9a27
JH Park <74255823+ParkJh38@users.noreply.github.com>
2024-10-29 16:17:37 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,7 +1,11 @@
 def solution(phone_number):
-    #for i in range(0, len(phone_number)-4) 
-    changed_num = '*' * (len(phone_number) - 4) + phone_number[-4:]
-    return changed_num
-
-# 전화번호 문자열로 주어짐
-# 전화번호 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가림 -> 가린 문자열 return 
+    phone = []
+    for i in phone_number:
+        phone.append(i)
+    print(phone)
+    for i in range(0,len(phone)-4):
+        phone[i] = '*' 
+    print(phone)
+    answer = ''.join(phone)
+    print(answer)
+    return answer
