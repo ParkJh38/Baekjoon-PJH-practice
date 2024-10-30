2e7593e864967b2e88c24aaa85626bb15ab938fe
JH Park <74255823+ParkJh38@users.noreply.github.com>
2024-10-31 01:03:01 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,12 +1,7 @@
 def solution(n):
     answer = 0
     str_n = str(n)
-    for i in range(len(str_n)):  #인덱스 사용
-        answer += int(str_n[i])
-    return answer
+    for i in str_n:
+        answer += int(i)
 
-# def solution(n):
-#     answer = 0
-#     for i in str(n):   #인덱스 사용하지 않고 각 자릿수 문자열로 파악
-#         answer += int(i)
-#     return answer
+    return answer
