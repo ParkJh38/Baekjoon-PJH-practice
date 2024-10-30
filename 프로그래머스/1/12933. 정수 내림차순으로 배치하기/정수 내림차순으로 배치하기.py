f01b742d561ea95ad19321ddb6193cd3f704f320
JH Park <74255823+ParkJh38@users.noreply.github.com>
2024-10-31 01:26:21 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,15 +1,10 @@
 def solution(n):
     answer = 0
-    #n을 arr로 변환한 후, sort() 한 다음, 다시 int로 변환
+    str_n = str(n)
     arr = []
     for i in str(n):
         arr.append(i)
-    print(arr)
     arr.sort()
     arr.reverse()
-    print(arr)
-    
-    #문자 리스트 arr를 하나의 문자열로 결합하고 이를 정수로 변환
-    #''는 결합구분자 형태 '-' -> 1-2-3으로 결합
     answer = int(''.join(arr))
     return answer
