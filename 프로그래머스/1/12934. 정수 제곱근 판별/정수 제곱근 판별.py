fe06bb5f21b18f09e7338d2f1483cf11c42b2bd8
JH Park <74255823+ParkJh38@users.noreply.github.com>
2024-10-31 01:46:04 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -3,7 +3,8 @@ import math
 def solution(n):
     answer = 0
     x = math.sqrt(n)
-    if x == int(x):
-        return ((x+1) ** 2)
+    if n == x**2 and int(x) == x:
+        return (x+1)**2
     else:
         return -1
+    return answer
